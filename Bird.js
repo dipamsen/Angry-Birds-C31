662d5d6040a4c7a458823976c287ea0654e114a9
dipamsen <dipamdiptam@gmail.com>
2020-03-16 18:43:19 +0530

    upload

--- Bird.js
+++ Bird.js
@@ -2,6 +2,9 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smoke = loadImage("sprites/smoke.png");
+    this.path = [];
+    
   }
 
   display() {
@@ -9,5 +12,13 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+    if(this.body.position.x>220 && this.body.velocity.x>10){
+      var pos = [this.body.position.x, this.body.position.y];
+      this.path.push(pos);
+    }
+    for(var j=0; j<this.path.length; j++){
+      image(this.smoke,this.path[j][0],this.path[j][1]);
+    }
+
   }
 }
